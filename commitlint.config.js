be54334913cae5857c3326dc649a6629e6751299
Johnson <zxmagichacker@gmail.com>
2019-06-12 11:45:39 +0800

    chore: 新增commit message的type

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"]
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+        "chore",
+        "library",
+        "build"
+      ]
+    ]
+  }
 };

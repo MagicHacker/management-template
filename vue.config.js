75c3803d2403648b6d729a5e8c525d70d0012514
Johnson <zxmagichacker@gmail.com>
2019-07-03 17:34:52 +0800

    feat: 配置svg-sprite-loader

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,24 @@
+const path = require("path");
+const resolve = dir => path.join(__dirname, dir);
 module.exports = {
   devServer: {
     before: require("./mock"),
     hotOnly: true
+  },
+  chainWebpack: config => {
+    config.module
+      .rule("svg")
+      .exclude.add(resolve("./src/icon"))
+      .end();
+    config.module
+      .rule("icon")
+      .test(/\.svg$/)
+      .include.add(resolve("./src/icon"))
+      .end()
+      .use("svg-sprite-loader")
+      .loader("svg-sprite-loader")
+      .options({
+        symbolId: "icon-[name]"
+      });
   }
 };

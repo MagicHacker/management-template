a21cdd09255d97b1a94844045bb6d2688f11c3f8
Johnson <zxmagichacker@gmail.com>
2019-07-04 19:44:55 +0800

    feat: 构建element icon组件

---
+++ src/components/element-icon.vue
@@ -0,0 +1,261 @@
+<template>
+  <div class="element-wrap">
+    <el-row class="icon-row">
+      <el-col :span="3">
+        <i class="el-icon-eleme"></i>
+        <span class="icon-name">el-icon-eleme</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-delete"></i>
+        <span class="icon-name">el-icon-delete</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-setting"></i>
+        <span class="icon-name">el-icon-setting</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-user"></i>
+        <span class="icon-name">el-icon-user</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-phone"></i>
+        <span class="icon-name">el-icon-phone</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-more"></i>
+        <span class="icon-name">el-icon-more</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-goods"></i>
+        <span class="icon-name">el-icon-goods</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-warning"></i>
+        <span class="icon-name">el-icon-warning</span>
+      </el-col>
+    </el-row>
+    <el-row class="icon-row">
+      <el-col :span="3">
+        <i class="el-icon-question"></i>
+        <span class="icon-name">el-icon-question</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-info"></i>
+        <span class="icon-name">el-icon-info</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-remove"></i>
+        <span class="icon-name">el-icon-remove</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-success"></i>
+        <span class="icon-name">el-icon-success</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-error"></i>
+        <span class="icon-name">el-icon-error</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-help"></i>
+        <span class="icon-name">el-icon-help</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-minus"></i>
+        <span class="icon-name">el-icon-minus</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-plus"></i>
+        <span class="icon-name">el-icon-plus</span>
+      </el-col>
+    </el-row>
+    <el-row class="icon-row">
+      <el-col :span="3">
+        <i class="el-icon-check"></i>
+        <span class="icon-name">el-icon-check</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-close"></i>
+        <span class="icon-name">el-icon-close</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-picture"></i>
+        <span class="icon-name">el-icon-picture</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-upload"></i>
+        <span class="icon-name">el-icon-upload</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-download"></i>
+        <span class="icon-name">el-icon-download</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-camera"></i>
+        <span class="icon-name">el-icon-camera</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-bell"></i>
+        <span class="icon-name">el-icon-bell</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-menu"></i>
+        <span class="icon-name">el-icon-menu</span>
+      </el-col>
+    </el-row>
+    <el-row class="icon-row">
+      <el-col :span="3">
+        <i class="el-icon-share"></i>
+        <span class="icon-name">el-icon-share</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-back"></i>
+        <span class="icon-name">el-icon-back</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-right"></i>
+        <span class="icon-name">el-icon-right</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-bottom"></i>
+        <span class="icon-name">el-icon-bottom</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-top"></i>
+        <span class="icon-name">el-icon-top</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-finished"></i>
+        <span class="icon-name">el-icon-finished</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-sort"></i>
+        <span class="icon-name">el-icon-sort</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-rank"></i>
+        <span class="icon-name">el-icon-rank</span>
+      </el-col>
+    </el-row>
+    <el-row class="icon-row">
+      <el-col :span="3">
+        <i class="el-icon-loading"></i>
+        <span class="icon-name">el-icon-loading</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-view"></i>
+        <span class="icon-name">el-icon-view</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-date"></i>
+        <span class="icon-name">el-icon-date</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-edit"></i>
+        <span class="icon-name">el-icon-edit</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-folder"></i>
+        <span class="icon-name">el-icon-folder</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-tickets"></i>
+        <span class="icon-name">el-icon-tickets</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-printer"></i>
+        <span class="icon-name">el-icon-printer</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-paperclip"></i>
+        <span class="icon-name">el-icon-paperclip</span>
+      </el-col>
+    </el-row>
+    <el-row class="icon-row">
+      <el-col :span="3">
+        <i class="el-icon-search"></i>
+        <span class="icon-name">el-icon-search</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-monitor"></i>
+        <span class="icon-name">el-icon-monitor</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-attract"></i>
+        <span class="icon-name">el-icon-attract</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-mobile"></i>
+        <span class="icon-name">el-icon-mobile</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-scissors"></i>
+        <span class="icon-name">el-icon-scissors</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-umbrella"></i>
+        <span class="icon-name">el-icon-umbrella</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-headset"></i>
+        <span class="icon-name">el-icon-headset</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-brush"></i>
+        <span class="icon-name">el-icon-brush</span>
+      </el-col>
+    </el-row>
+    <el-row class="icon-row">
+      <el-col :span="3">
+        <i class="el-icon-document"></i>
+        <span class="icon-name">el-icon-document</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-mouse"></i>
+        <span class="icon-name">el-icon-mouse</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-coordinate"></i>
+        <span class="icon-name">el-icon-coordinate</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-reading"></i>
+        <span class="icon-name">el-icon-reading</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-film"></i>
+        <span class="icon-name">el-icon-film</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-suitcase"></i>
+        <span class="icon-name">el-icon-suitcase</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-receiving"></i>
+        <span class="icon-name">el-icon-receiving</span>
+      </el-col>
+      <el-col :span="3">
+        <i class="el-icon-collection"></i>
+        <span class="icon-name">el-icon-collection</span>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<script>
+export default {
+  name: "ElementIcon"
+};
+</script>
+<style lang="scss">
+.element-wrap {
+  .icon-row {
+    margin-top: 25px;
+    i {
+      display: block;
+      font-size: 30px;
+    }
+    .icon-name {
+      display: inline-block;
+    }
+  }
+}
+</style>

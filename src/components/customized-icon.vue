ea3016d9b54af74f83230c093d13bbcce2fde61a
Johnson <zxmagichacker@gmail.com>
2019-07-06 18:25:49 +0800

    feat: 构建自定义图标页面

--- src/components/customized-icon.vue
+++ src/components/customized-icon.vue
@@ -1,241 +1,465 @@
 <template>
   <div class="customized-wrap">
-    <el-row>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+    <el-row class="icon-row">
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-animation"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-animation</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-art"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-art</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-book"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-book</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-calender"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-calender</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-calligraphy"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-calligraphy</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-colleagecap"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-colleagecap</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-count"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-count</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
-      </el-col>
-    </el-row>
-    <el-row>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
-      </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
-      </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
-      </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
-      </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
-      </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
-      </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
-      </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-crown"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-crown</span>
+        </div>
       </el-col>
     </el-row>
-    <el-row>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+    <el-row class="icon-row">
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-dictation"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-dictation</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-drawing"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-drawing</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-listen"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-listen</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-log"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-log</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-metal"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-metal</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-ppt"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-ppt</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-rank"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-rank</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
-      </el-col>
-    </el-row>
-    <el-row>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
-      </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
-      </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
-      </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
-      </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
-      </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
-      </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
-      </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-search"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-search</span>
+        </div>
       </el-col>
     </el-row>
-    <el-row>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+    <el-row class="icon-row">
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-speak"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-speak</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-swim"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-swim</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-word"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-word</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-written"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-written</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-air"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-air</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-browse"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-browse</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-calculator"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-calculator</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
-      </el-col>
-    </el-row>
-    <el-row>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
-      </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
-      </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
-      </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
-      </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
-      </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
-      </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
-      </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-camera"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-camera</span>
+        </div>
       </el-col>
     </el-row>
-    <el-row>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+    <el-row class="icon-row">
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-car"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-car</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-castle"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-castle</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-clock"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-clock</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-feedback"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-feedback</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-files"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-files</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-food"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-food</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-gallery"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-gallery</span>
+        </div>
       </el-col>
-      <el-col>
-        <icon-svg></icon-svg>
-        <span></span>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-hotel"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-hotel</span>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row class="icon-row">
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-joke"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-joke</span>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-mic"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-mic</span>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-news"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-news</span>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-oil"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-oil</span>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-recorder"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-recorder</span>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-remind"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-remind</span>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-video"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-video</span>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-swf"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-swf</span>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row class="icon-row">
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-awake"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-awake</span>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-flight"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-flight</span>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-chat"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-chat</span>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-css"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-css</span>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-music"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-music</span>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-nav"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-nav</span>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-train"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-train</span>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-excel"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-excel</span>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row class="icon-row">
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-html"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-html</span>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-java"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-java</span>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-note"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-note</span>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-pdf"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-pdf</span>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-voice"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-voice</span>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-rar"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-rar</span>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-txt"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-txt</span>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <div class="icon-svg">
+          <icon-svg symbol="icon-zip"></icon-svg>
+        </div>
+        <div>
+          <span class="icon-name">icon-zip</span>
+        </div>
       </el-col>
     </el-row>
   </div>
@@ -247,5 +471,11 @@ export default {
 </script>
 <style lang="scss">
 .customized-wrap {
+  .icon-row {
+    margin-top: 25px;
+    .icon-svg {
+      font-size: 30px;
+    }
+  }
 }
 </style>

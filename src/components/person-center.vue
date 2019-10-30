1b9784063520f58752f9fa41fd07720d3e4acb9f
Johnson <zxmagichacker@gmail.com>
2019-10-30 20:50:59 +0800

    refactor: 重构个人中心页面

--- src/components/person-center.vue
+++ src/components/person-center.vue
@@ -1,87 +1,120 @@
 <template>
   <div class="person-wrap">
-    <el-card class="person-detail">
-      <div>
-        <img src="../assets/Belle.jpg" alt />
-        <h4>MagicHacker</h4>
-      </div>
-      <div class="person-connect">
-        <h4>Connect:</h4>
-        <ul>
-          <li>
-            <el-row>
-              <el-col :span="6">
-                <icon-svg symbol="icon-twitter"></icon-svg>
-              </el-col>
-              <el-col :span="16">xxx</el-col>
-            </el-row>
-          </li>
-          <li>
-            <el-row>
-              <el-col :span="6">
-                <icon-svg symbol="icon-facebook"></icon-svg>
-              </el-col>
-              <el-col :span="16">xxxx</el-col>
-            </el-row>
-          </li>
-          <li>
-            <el-row>
-              <el-col :span="6">
-                <icon-svg symbol="icon-instagram"></icon-svg>
-              </el-col>
-              <el-col :span="16">xxx</el-col>
-            </el-row>
-          </li>
-          <li>
-            <el-row>
-              <el-col :span="6">
-                <icon-svg symbol="icon-phone"></icon-svg>
-              </el-col>
-              <el-col :span="16">xxx</el-col>
-            </el-row>
-          </li>
-        </ul>
-      </div>
-      <div class="person-about">
-        <h4>About:</h4>
-        <p>
-          I am a web developer and desginer based in Medellín - Colombia, I like
-          read books, good music and nature.
-        </p>
-      </div>
-      <div class="person-social">
-        <h4>Social:</h4>
-        <el-button-group>
-          <el-button type="warning">Facebook</el-button>
-          <el-button type="primary">Twitter</el-button>
-          <el-button type="danger">Gmail</el-button>
-        </el-button-group>
-      </div>
-    </el-card>
-    <el-card class="person-setting">
-      <h4>资料设置</h4>
-      <el-form label-width="80px">
-        <el-form-item label="Name">
-          <el-input></el-input>
-        </el-form-item>
-        <el-form-item label="Twitter">
-          <el-input></el-input>
-        </el-form-item>
-        <el-form-item label="FaceBook">
-          <el-input></el-input>
-        </el-form-item>
-        <el-form-item label="Phone">
-          <el-input></el-input>
-        </el-form-item>
-        <el-form-item label="Instagram">
-          <el-input></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary">提交</el-button>
-          <el-button>重置</el-button>
-        </el-form-item>
-      </el-form>
-    </el-card>
+    <el-row :gutter="20">
+      <el-col :span="8">
+        <el-card>
+          <div slot="header">
+            <span>个人简介</span>
+          </div>
+          <div class="person-avatar">
+            <img src="../assets/Belle.jpg" alt />
+          </div>
+          <div class="person-name">
+            <span>MagicHacker</span>
+          </div>
+          <div class="person-connect">
+            <div class="connect-title">
+              <icon-svg symbol="icon-connect" class="icon-svg"></icon-svg>
+              <div>
+                <span>Connect</span>
+              </div>
+            </div>
+            <ul class="connect-list">
+              <li>
+                <el-row>
+                  <el-col :span="6">
+                    <icon-svg symbol="icon-twitter"></icon-svg>
+                  </el-col>
+                  <el-col :span="16">xxx</el-col>
+                </el-row>
+              </li>
+              <li>
+                <el-row>
+                  <el-col :span="6">
+                    <icon-svg symbol="icon-facebook"></icon-svg>
+                  </el-col>
+                  <el-col :span="16">xxxx</el-col>
+                </el-row>
+              </li>
+              <li>
+                <el-row>
+                  <el-col :span="6">
+                    <icon-svg symbol="icon-instagram"></icon-svg>
+                  </el-col>
+                  <el-col :span="16">xxx</el-col>
+                </el-row>
+              </li>
+              <li>
+                <el-row>
+                  <el-col :span="6">
+                    <icon-svg symbol="icon-phone"></icon-svg>
+                  </el-col>
+                  <el-col :span="16">xxx</el-col>
+                </el-row>
+              </li>
+            </ul>
+          </div>
+          <div class="person-about">
+            <div class="about-title">
+              <icon-svg symbol="icon-about" class="icon-svg"></icon-svg>
+              <div>
+                <span>About</span>
+              </div>
+            </div>
+            <div>
+              <span>
+                I am a web developer and desginer based in Medellín - Colombia,
+                I like read books, good music and nature.
+              </span>
+            </div>
+          </div>
+          <div class="person-social">
+            <div class="social-title">
+              <icon-svg symbol="icon-social" class="icon-svg"></icon-svg>
+              <div>
+                <span>Social</span>
+              </div>
+            </div>
+            <div class="social-btn">
+              <el-button type="warning">Facebook</el-button>
+              <el-button type="primary">Twitter</el-button>
+              <el-button type="danger">Gmail</el-button>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="16">
+        <el-card>
+          <el-tabs>
+            <el-tab-pane label="Activity">Activity</el-tab-pane>
+            <el-tab-pane label="Timeline">Timeline</el-tab-pane>
+            <el-tab-pane label="Account">
+              <el-form label-width="80px">
+                <el-form-item label="Name">
+                  <el-input></el-input>
+                </el-form-item>
+                <el-form-item label="Twitter">
+                  <el-input></el-input>
+                </el-form-item>
+                <el-form-item label="FaceBook">
+                  <el-input></el-input>
+                </el-form-item>
+                <el-form-item label="Phone">
+                  <el-input></el-input>
+                </el-form-item>
+                <el-form-item label="Instagram">
+                  <el-input></el-input>
+                </el-form-item>
+                <el-form-item>
+                  <el-button type="primary">提交</el-button>
+                  <el-button>重置</el-button>
+                </el-form-item>
+              </el-form>
+            </el-tab-pane>
+          </el-tabs>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 <script>
@@ -96,23 +129,52 @@ export default {
   box-sizing: border-box;
   padding: 15px;
   position: relative;
-  .person-detail {
-    width: 50%;
-    float: left;
-    height: 100%;
-    margin-right: 15px;
-    padding: 15px;
-    .person-connect {
-      text-align: left;
+  .person-avatar,
+  .person-name {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
+  .person-name {
+    height: 30px;
+    color: #333;
+    font-weight: 700;
+    font-size: 16px;
+    margin-top: 10px;
+  }
+  .icon-svg {
+    font-size: 12px;
+    margin-right: 5px;
+  }
+  .connect-title,
+  .about-title,
+  .social-title {
+    width: 100%;
+    height: 30px;
+    border-bottom: 1px solid #dfe6ec;
+    font-weight: 700;
+    color: #606266;
+    padding-bottom: 5px;
+    margin-bottom: 5px;
+    display: flex;
+    align-items: center;
+  }
+  .person-connect {
+    width: 100%;
+    .connect-list {
+      width: 100%;
+      margin-top: 10px;
     }
-    .person-about {
-      text-align: left;
-      p {
-        margin: 0px;
-      }
-    }
-    .person-social {
-      text-align: left;
+  }
+  .person-about {
+    width: 100%;
+  }
+  .person-social {
+    width: 100%;
+    .social-btn {
+      margin-top: 10px;
+      display: flex;
+      justify-content: space-between;
     }
   }
   .person-setting {
